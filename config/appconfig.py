b3b07aa76c2d5a50bbc4a1f078a189622052f0a3
juxiangwu <kkoolerter@gmail.com>
2018-02-12 00:35:44 +0800

    new code

--- config/appconfig.py
+++ config/appconfig.py
@@ -16,7 +16,7 @@ app = Flask(__name__,static_folder=static_dir,template_folder=template_dir)
 cacheClient = memcache.Client(servers=['127.0.0.1:11211'])
 # dialect+driver://username:password@host:port/database?charset=utf8
 # 配置 sqlalchemy  数据库驱动://数据库用户名:密码@主机地址:端口/数据库?编码
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:356007@127.0.0.1:3306/pss?charset=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://<dbuser>:<password>@dbhost:port/pss?charset=utf8'
 # 初始化
 db = SQLAlchemy(app)
 cache = Cache(config={'CACHE_TYPE': 'memcached',
